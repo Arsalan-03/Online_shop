288c16485d3d573c6e2a66a4cfbbac810814d174
Arsalan Arbalzhinov <buryat_sambist03@mail.ru>
2023-12-14 02:51:14 +0800

    add debug

--- app/public/index.php
+++ app/public/index.php
@@ -1,19 +1,9 @@
 <?php
+$a = 5;
+$b = 10;
 
-//if (empty($errors)) {
-//    $statement = $pdo->prepare("insert into users (name, email, password) values (:name, :email, :password)");
-//    $statement->execute(['name' => $name, 'email' => $email, 'password' => $password]);
-//
-//    $statement = $pdo->prepare('SELECT * FROM users WHERE email = :email');
-//    $statement->execute(['email' => $email]);
-//    $result = $statement->fetch(PDO::FETCH_ASSOC);
-//    print_r($result);
-//} else {
-//    foreach($errors as $key) {
-//        foreach($key as $message) {
-//            echo $message . "<br>";
-//        }
-//    }
+ $sum = $a + $b;
+
 //}
 //try {
 //    $pdo = new PDO("pgsql:host=db;port=5432;dbname=room_bd;", "arsik", "0000");
