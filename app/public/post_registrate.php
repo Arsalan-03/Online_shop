7a751dcb8bf3cd4fe911797c34490c9b44373dff
Arsalan Arbalzhinov <buryat_sambist03@mail.ru>
2023-12-13 21:08:17 +0800

    Валидация

--- app/public/post_registrate.php
+++ app/public/post_registrate.php
@@ -2,43 +2,57 @@
 
 print_r($_POST);
 
+$pdo = new PDO("pgsql:host=db;port=5432;dbname=postgres;", "arsik", "0000");
+
+$errors = [];
+
 $name = $_POST['name'];
- if(strlen($name) < 1) {
-    echo 'Имя должен быть больше 2 символов';
+ if(strlen($name) < 2) {
+    $errors['name'][] = 'Имя должен быть больше 2 символов';
 }
 
 $email = $_POST['email'];
- if(strlen($email) < 1) {
-     echo 'email должен быть больше 2 символов';
+ if(strlen($email) < 2) {
+     $errors['email'][] = 'email должен быть больше 2 символов';
  }
  $str = "@";
  $pos = strpos($email, $str);
 
- if($pos !== true) {
-     echo "email должен содержать символ @ в строке";
+ if($pos === false) {
+     $errors['email'][] = "email должен содержать символ @ в строке";
  }
 
  $password = $_POST['psw'];
  $password_repeat = $_POST['psw-repeat'];
 
- if(strlen($password) < 1) {
-     echo 'Пароль должен быть больше 2 символов';
+ if(strlen($password) < 2) {
+     $errors['password'][] = 'Пароль должен быть больше 2 символов';
  }
 
  if($password !== $password_repeat) {
-     echo 'Пароли не совпадают';
+     $errors['password'][] = 'Пароли не совпадают';
  }
 
-$pdo = new PDO("pgsql:host=db;port=5432;dbname=postgres;", "arsik", "0000");
+ if (empty($errors)) {
+     $statement = $pdo->prepare("insert into users (name, email, password) values (:name, :email, :password)");
+     $statement->execute(['name' => $name, 'email' => $email, 'password' => $password]);
+ } else {
+     foreach($errors as $key) {
+         foreach($key as $message) {
+             echo $message . "<br>";
+         }
+     }
+ }
+
 
 //print_r($pdo);
 
-$statement = $pdo->prepare("insert into users (name, email, password) values (:name, :email, :password)");
-$statement->execute(['name' => $name, 'email' => $email, 'password' => $password]);
+//$statement = $pdo->prepare("insert into users (name, email, password) values (:name, :email, :password)");
+//$statement->execute(['name' => $name, 'email' => $email, 'password' => $password]);
 
 
 #Выводит всех пользователей в таблице users
-$statement = $pdo->prepare('SELECT * FROM users WHERE name = :name');
-$statement->execute(['name' => $name]);
-$result = $statement->fetch();
-print_r($result);
+//$statement = $pdo->prepare('SELECT * FROM users WHERE name = :name');
+//$statement->execute(['name' => $name]);
+//$result = $statement->fetch();
+//print_r($result);
