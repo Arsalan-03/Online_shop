80a471f87eb68b633f88658753c7665135792989
Arsalan Arbalzhinov <buryat_sambist03@mail.ru>
2023-12-15 04:09:29 +0800

    Create get/post login.php

--- app/public/get_registrate.php
+++ app/public/get_registrate.php
@@ -5,15 +5,19 @@
         <hr>
 
         <label for="name"><b>Name</b></label>
+        <label style="color: red"><?php echo $errors['name'] ?? ''; ?></label>
         <input type="text" placeholder="Enter Name" name="name" id="name" required>
 
         <label for="email"><b>Email</b></label>
+        <label style="color: red"><?php echo $errors['email'] ?? ''; ?></label>
         <input type="text" placeholder="Enter Email" name="email" id="email" required>
 
         <label for="psw"><b> </b></label>
+        <label style="color: red"><?php echo $errors['psw'] ?? ''; ?></label>
         <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
 
         <label for="psw-repeat"><b>Repeat Password</b
+            <label style="color: red"><?php echo $errors['psw-repeat'] ?? ''; ?></label>
             ></label><input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
         <hr
 
